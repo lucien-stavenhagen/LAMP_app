56fc30f32b58c39800463869fb69ccd183b514a7
luciens <luciens@luciens-Virtual-Machine.mshome.net>
2019-12-07 11:04:10 -0600

    add encryption and decryption

--- inc/connector.php
+++ inc/connector.php
@@ -7,6 +7,21 @@ $dbPassword = "dalemace";
 $dbName = "lampdemo";
 $tableName = "books";
 
+$encThumbprint = "abc-123+";
+
+function encryptText($data, $thumbPrint){
+	$enc_key=base64_decode($thumbPrint);
+	$iv=openssl_random_pseudo_bytes(openssl_cipher_iv_length('aes-256-cbc'));
+	$encrypted=openssl_encrypt($data, 'aes-256-cbc',$enc_key, 0, $iv);
+	return base64_encode($encrypted . "::" . $iv);
+}
+
+function decryptText($data, $thumbPrint){
+	$enc_key=base64_decode($thumbPrint);
+	list($encrypted,$iv)=array_pad(explode('::', base64_decode($data), 2),2,null);
+	return openssl_decrypt($encrypted, 'aes-256-cbc', $enc_key, 0, $iv);
+}
+
 function connectToDb($dbServername, $dbUsername, $dbPassword, $dbName)
 {
 	$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
@@ -21,8 +36,8 @@ function createTable($conn, $tableName)
 	$ok = true;
 	$maintable = "create table if not exists " . $tableName . " (
 		id int(6) unsigned auto_increment primary key,
- 		title varchar(255),
- 		author varchar(255)
+ 		title text,
+ 		author text
 	)";
 	return $conn->query($maintable);
 }
@@ -34,7 +49,7 @@ function getAllBooks($conn, $tableName)
 	if ($result->num_rows > 0) {
 		$text = "<table id='results-table'><tr><th>ID</th><th>Title</th><th>Author</th></tr>";
 		while ($row = $result->fetch_assoc()) {
-			$text .= "<tr><td>" . $row['id'] . "</td><td>" . $row['title'] . "</td><td>" . $row['author'] . "</td></tr>";
+			$text .= "<tr><td>" . $row['id'] . "</td><td>" . decryptText($row['title'],$encThumbprint) . "</td><td>" . decryptText($row['author'],$thumbPrint) . "</td></tr>";
 		}
 		$text .= "</table>";
 		return $text;
@@ -45,7 +60,7 @@ function getAllBooks($conn, $tableName)
 
 function addBook($conn, $tableName, $title, $author)
 {
-	$sql = "insert into " . $tableName . " ( title, author ) values ( '" . $title . "', '" . $author . "' )";
+	$sql = "insert into " . $tableName . " ( title, author ) values ( '" .encryptText($title,$encThumbprint) . "', '" .encryptText($author,$encThumbprint) . "' )";
 	return $conn->query($sql);
 }
 
@@ -56,6 +71,9 @@ function deleteBook($conn, $tableName, $id)
 }
 
 function updateBook($conn, $tableName, $id, $newTitle, $newAuthor){
+	 $newTitle = encryptText($newTitle,$encThumbprint);
+	 $newAuthor = encryptText($newAuthor,$encThumbprint);
+	 
 	$sql="update {$tableName} set title='{$newTitle}', author='{$newAuthor}' where id={$id}";
 	return $conn->query($sql);
 }
